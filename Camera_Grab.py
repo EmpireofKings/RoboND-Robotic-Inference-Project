06686f331b76cc3efd5c669cc5525ad619ecaf04
Muthanna A. Attyah <mkhuthir@gmail.com>
2018-04-10 20:03:05 +0400

    chore: cleaned python code

--- Camera_Grab.py
+++ Camera_Grab.py
@@ -5,8 +5,8 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 number = 0
-set_dir   = 'AED'
-name_type = 'AED_'
+set_dir   = 'AED500'
+name_type = 'AED500_front'
 
 print ("Photo capture enabled! Press esc to take photos!")
 
@@ -15,11 +15,12 @@ while True:
     cv2.imshow('Color Picture', frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
+        fname = 'Data/' + set_dir + '/' + name_type + "_" + str(number) + ".png"
         #gray   = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         #resize = cv2.resize(frame,(360,360), interpolation = cv2.INTER_NEAREST)
-        cv2.imwrite('Data/' + set_dir + '/' + name_type + "_" + str(number) + ".png", frame)
-        print ("Saving image number: " + str(number))
-	print (frame.shape)
+        cv2.imwrite(fname , frame)
+        print ("Saving image : "+ str(fname))
+        print (frame.shape)
         number+=1
 
     if number == 100:
